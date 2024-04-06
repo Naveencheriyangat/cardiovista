99973526ce7f51d3bad6534556a04287d3cd8810
Naveencheriyangat <naveenkkut128@gmail.com>
2024-04-06 09:52:33 +0530

    image added

--- lib/skip3.dart
+++ lib/skip3.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'start.dart';
+import 'skip2.dart'; // Import the skip2.dart file
 
 void main() {
-  runApp(const last());
+  runApp(const Last());
 }
 
-class last extends StatelessWidget {
-  const last({Key? key}) : super(key: key);
+class Last extends StatelessWidget {
+  const Last({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,78 +25,83 @@ class last extends StatelessWidget {
 class CardioVistaHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Center(
-              child: Image.asset(
-                'assets/images/main3.png', // Provide your image path here
-                width: 200, // Adjust width as needed
-                height: 200, // Adjust height as needed
+    return GestureDetector(
+      onHorizontalDragEnd: (details) {
+        if (details.primaryVelocity != null) {
+          // Detect if drag velocity is positive (swiping from left to right)
+          if (details.primaryVelocity! > 0) {
+            // Swipe from left to right, navigate to skip2.dart
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Frontt()),
+            );
+          } else {
+            // Swipe from right to left, navigate to previous page
+            Navigator.pop(context);
+          }
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Center(
+                child: Image.asset(
+                  'assets/images/main3.png', // Provide your image path here
+                  width: 400, // Adjust width as needed
+                  height: 400, // Adjust height as needed
+                ),
               ),
             ),
-          ),
-           SizedBox(height: 30,),
-          Expanded(
-            child: Stack(
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/images/ellip.png', // Provide the path for the Ellipse image here
-                    width: 400, // Adjust width as needed
-                    height: 400, // Adjust height as needed
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    "Uncover your heart health, just for you",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+            SizedBox(height: 10,),
+            Expanded(
+              child: Stack(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/images/ellip.png', // Provide the path for the Ellipse image here
+                      width: 400, // Adjust width as needed
+                      height: 400, // Adjust height as needed
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {
-                   //  Navigator.push(
-                      //    context,
-                       //   MaterialPageRoute(builder: (context) => CardioVistaApp()),
-                       // );// Add functionality for 'skip' button here
-                  },
-                  child: Text(
-                    'Skip',
-                    style: TextStyle(color: Colors.black),
+                  Center(
+                    child: Text(
+                      "Uncover your heart health, just for you",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                     Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Login()),
-                        );// Add functionality for 'next' button here
-                  },
-                  child: Text(
-                    'Next',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Login()),
+                      );
+                    },
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
