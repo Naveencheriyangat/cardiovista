55f5e909abbbbf2b1aeb6e0ea7fb84b16b6e27aa
Naveencheriyangat <naveenkkut128@gmail.com>
2024-04-06 09:52:16 +0530

    image added

--- lib/reg.dart
+++ lib/reg.dart
@@ -26,19 +26,25 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text('Cardio ', style: TextStyle(color: Colors.black)),
-            Text('Vista', style: TextStyle(color: Color(0xFFAA1F24))),
-          ],
-        ),
-        elevation: 5,
-        shadowColor: Colors.black,
-         leading: Container(
-          child: Image.asset('assets/images/Logo.png')),
-      ),
+      appBar:AppBar(
+  title: Row(
+    mainAxisAlignment: MainAxisAlignment.end,
+    children: [
+      Text('Cardio ', style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold)),
+      Text('Vista', style: TextStyle(color: Color(0xFFAA1F24), fontSize: 18.0, fontWeight: FontWeight.bold)),
+    ],
+  ),
+  elevation: 5,
+  shadowColor: Colors.black,
+  leading: Container(
+    padding: EdgeInsets.all(8.0), // Add padding for better clarity
+    child: Image.asset(
+      'assets/images/Logo.png',
+      height: 40, // Adjust the height as needed for clarity
+    ),
+  ),
+),
+
       body: Center(
         child: MyLoginPage(),
       ),
